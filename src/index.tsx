603697cc513280115ff4eec83a5a2d273d22d5ef
Luneciux <jejosu346@gmail.com>
2023-02-17 11:06:59 -0300

    organizing the routes

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Menu from './pages/Menu';
 import Home from 'pages/Home';
+import AppRouter from 'routes';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,6 +11,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <Home />
+    <AppRouter />
   </React.StrictMode>
 );
